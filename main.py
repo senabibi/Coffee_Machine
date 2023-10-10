a591f768bf8c720b4ed9754f23b1794e532b58e4
NURSENA BİTİRGEN <94117345+senabibi@users.noreply.github.com>
2023-10-10 15:08:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
 money_machine=MoneyMachine()
-coffe_maker=CoffeeMaker()
+coffee_maker=CoffeeMaker()
 menu=Menu()
-
 is_on=True
-
 
 while is_on:
     options=menu.get_items()
-    choice=input(f"What would you like? ({options})").lower()
+    choice=input(f"What would you like ?({options})")
     if choice=="off":
         is_on=False
     elif choice=="report":
-        coffe_maker.report() 
-        money_machine.report()   
-    else:
+        coffee_maker.report()
+        money_machine.report()
+    else:        
         drink=menu.find_drink(choice)
-        print(coffe_maker.is_resource_sufficient(drink))
-
-
-
-
+        if coffee_maker.is_resource_sufficient(drink) and (money_machine.make_payment(drink.cost)):
+            coffee_maker.make_coffee(drink)
